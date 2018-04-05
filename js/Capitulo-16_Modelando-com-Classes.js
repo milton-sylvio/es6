333a5b66130aca79b458669098f5cc0a3f313bf5
Milton Sylvio <milton.sylvio@gads.com.br>
2018-04-05 11:52:53 -0300

    Capitulo 16

---
+++ js/Capitulo-16_Modelando-com-Classes.js
@@ -0,0 +1,69 @@
+/**************************************************************************************************
+ * CAPÍTULO 16
+ * 
+ * MODELANDO COM CLASSES
+ * 
+ **************************************************************************************************/
+function Carro(modelo, chassi, qtd_portas) {
+    this.modelo = modelo;
+    this.chassi = chassi;
+    this.qtd_portas = qtd_portas;
+}
+
+const prototipo = new Carro('prototipo', '1290381209', 2);
+/* console.log(prototipo.modelo);
+console.log(prototipo.chassi);
+console.log(prototipo.qtd_portas);*/
+
+Carro.prototype.andar = function () {
+    console.log('vrum vrum');
+}
+
+//prototipo.andar();
+
+function Sonix(modelo, chassi, qtd_portas) {
+    Carro.call(this, modelo, chassi, qtd_portas);
+}
+Sonix.prototype = Object.create(Carro.prototype);
+Sonix.prototype.constructor = Sonix;
+
+const sonix = new Sonix('Sonix', '9120219', 4);
+// console.log(sonix.modelo);
+// sonix.andar();
+
+/* NOVA ABORDAGEM, DO ES6 */
+class Car {
+    constructor(model, chassi, doors) {
+        this.model = model;
+        this.chassi = chassi;
+        this.doors = doors;
+    }
+
+    walking () {
+        console.log('vrum vrum');
+    }
+}
+
+const car = new Car('Fiesta', '123654', 2);
+// console.log(car.model);
+// console.log(car.chassi);
+// console.log(car.doors);
+// car.walking();
+
+/*
+ * 16.2	ESTENDENDO CLASSES
+ **/
+class SUV extends Car {
+    constructor(model, chassi, doors){
+        super(model, chassi, doors);
+    }
+
+    cambio () {
+        console.log('Todos tem câmbio automático!');
+    }
+}
+
+const suv = new SUV('IZ35', '69853213', 4);
+suv.cambio();
+suv.walking();
+console.log(suv.model);
